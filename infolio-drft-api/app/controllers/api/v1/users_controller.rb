ddf5c5381ddbc4a7ec3b800eb3d4e5255cb5e377
Pascal Rassaby <pasbynumbers@gmail.com>
2020-09-21 22:24:59 +0100

    user auth features inplemented serverside

--- infolio-drft-api/app/controllers/api/v1/users_controller.rb
+++ infolio-drft-api/app/controllers/api/v1/users_controller.rb
@@ -1,17 +1,23 @@
 class Api::V1::UsersController < ApplicationController
+  skip_before_action :authorized, only: %i[create]
+
   def index
     @users = User.order(:id)
-    render json: { users: UserSerializer.new(@users) }, 
-    status: :accepted
+    render json: 
+    { users: UserSerializer.new(@users) }, 
+    status: :ok
   end
 
   def create
     @user = User.create(user_params)
     if @user.valid?
-      render json: { user: UserSerializer.new(@user) }, 
-      status: :accepted
+      @token = encode_token(user_id: @user.id)
+      render json: 
+      { user: UserSerializer.new(@user), jwt: @token }, 
+      status: :created
     else
-      render json: { error: 'failed to create user' }, 
+      render json: 
+      { error: 'failed to create user' }, 
       status: :not_acceptable
     end
   end
@@ -20,15 +26,14 @@ class Api::V1::UsersController < ApplicationController
 
   def user_params
     params
-    .require(:user)
-    .permit(
-      :username, 
-      :password, 
-      :first_name, 
-      :last_name, 
-      :email, 
-      :country
-    )
+      .require(:user)
+      .permit(
+        :username,
+        :password,
+        :first_name,
+        :last_name,
+        :email,
+        :country
+      )
   end
-  
 end
