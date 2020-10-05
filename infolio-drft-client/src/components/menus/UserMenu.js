39604b1ea30d873f871a230c9543453e0f46d0bc
Pascal Rassaby <pasbynumbers@gmail.com>
2020-10-05 20:55:28 +0100

    built sign up modal

--- infolio-drft-client/src/components/menus/UserMenu.js
+++ infolio-drft-client/src/components/menus/UserMenu.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
-import Grow from '@material-ui/core/Grow';
-import Paper from '@material-ui/core/Paper';
-import Popper from '@material-ui/core/Popper';
-import MenuItem from '@material-ui/core/MenuItem';
-import MenuList from '@material-ui/core/MenuList';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import UserRegister from "./modals/UserRegister";
+import Paper from "@material-ui/core/Paper";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
+    display: "flex",
   },
   paper: {
     marginRight: theme.spacing(2),
@@ -22,25 +19,24 @@ export default function UserMenu() {
   const [open, setOpen] = React.useState(false);
   const anchorRef = React.useRef(null);
 
-
-
   return (
     <div className={classes.root}>
       <Paper className={classes.paper}>
         <MenuList>
-        <MenuItem>Home</MenuItem>
-          { 
-            !!localStorage.token ? 
-              <div>
-               <MenuItem>Profile</MenuItem>
-               <MenuItem>Log Out</MenuItem> 
-              </div>
-            :
-            <MenuItem>Log In</MenuItem>
-          }
+          <MenuItem>Home</MenuItem>
+          {!!localStorage.token ? (
+            <div>
+              <MenuItem>Profile</MenuItem>
+              <MenuItem>Log Out</MenuItem>
+            </div>
+          ) : (
+            <div>
+                <UserRegister />
+              <MenuItem>Log In</MenuItem>
+            </div>
+          )}
         </MenuList>
       </Paper>
-      
     </div>
-  )
-        }
+  );
+}
